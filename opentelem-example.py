c2e9f764bb81c89d0a28f439671f908e1c226c88
Christopher Sharkey <ev2900@gmail.com>
2025-06-09 12:17:39 -0400

    Update opentelem-example.py

--- opentelem-example.py
+++ opentelem-example.py
@@ -1,3 +1,6 @@
+# Example command to run jaeger
+# docker run -d --name jaeger -e COLLECTOR_OTLP_ENABLED=true -p 16686:16686 -p 4317:4317 -p 4318:4318 jaegertracing/all-in-one:latest
+
 # OpenTelem trace code ...
 from opentelemetry import trace
 from opentelemetry.sdk.trace import TracerProvider
